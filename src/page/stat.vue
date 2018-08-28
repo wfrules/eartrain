a69efc38dd34c9e6fe6ae33f86ef646c0b49b6d0
wfrules <37941044@qq.com>
2018-08-28 23:20:12 +0800

    改善统计功能

--- src/page/stat.vue
+++ src/page/stat.vue
@@ -4,8 +4,12 @@
         <div>正确数{{score.right}}</div>
         <div>错误数{{score.wrong}}</div>  
         <div>连击数{{score.combo}}</div>
-        <div>正确率{{rate}}%</div>  
-        <x-button type="warn" @click.native="reset">清空</x-button>      
+        <div>正确率{{rate}}%</div>
+        <div>平均播放次数{{score.avg_played}}</div>
+      <div v-for="apool in score.pool_result">{{apool.caption}}
+          <span>正确{{apool.right}}</span><span>错误{{apool.wrong}}</span>
+      </div>
+      <x-button type="warn" @click.native="reset">清空</x-button>
     </div>
 </template>
 
@@ -27,9 +31,11 @@
         data(){
             return {
                 score: {
-                    right: 0,
-                    wrong: 0,
-                    combo: 0,
+                    right: '',
+                    wrong: '',
+                    combo: '',
+                    avg_played: '',
+                    pool_result: [],
                 },
             }
         },
@@ -39,13 +45,18 @@
                 objOptions.action = '获取分数';
                 objOptions.json = {};
                 objOptions.func = (json) => {
-                    this.score.right = Number(json.right);
-                    this.score.wrong = Number(json.wrong);
-                    this.score.combo = Number(json.combo);
+                    this.setScore(json);
                 }
                 this.request(objOptions);
         },
         methods: {
+            setScore(json){
+              this.score.right = Number(json.right);
+              this.score.wrong = Number(json.wrong);
+              this.score.combo = Number(json.combo);
+              this.score.avg_played = Number(json.avg_played);
+              this.score.pool_result = json.pool_result;
+            },
             reset(){
                 if (confirm('确认清空吗?'))
                 {
@@ -54,8 +65,7 @@
                     objOptions.action = '清空分数';
                     objOptions.json = {};
                     objOptions.func = (json) => {
-                        this.score.right = Number(json.right);
-                        this.score.wrong = Number(json.wrong);                    
+                        this.setScore(json);
                         alert('清空完毕');
                     }
                     this.request(objOptions);
